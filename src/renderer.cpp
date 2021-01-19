002aeaef9c2db4a401ebc97dc7b201446317869a
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-01-19 07:30:45 +0100

    Passing snake to renderer by reference

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -38,7 +38,7 @@ Renderer::~Renderer() {
   SDL_Quit();
 }
 
-void Renderer::Render(Snake const snake, SDL_Point const &food) {
+void Renderer::Render(Snake const &snake, SDL_Point const &food) {
   SDL_Rect block;
   block.w = screen_width / grid_width;
   block.h = screen_height / grid_height;
