c67d132b43748c2ab7b67912af7e7f6a9b8f964d
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-01-19 07:33:52 +0100

    Matched init order with declaration order

--- src/snake.h
+++ src/snake.h
@@ -9,11 +9,11 @@ class Snake {
   enum class Direction { kUp, kDown, kLeft, kRight };
 
   Snake(int grid_width, int grid_height)
-      : grid_width(grid_width),
-        grid_height(grid_height),
-        head_x(grid_width / 2),
-        head_y(grid_height / 2) {}
-
+      : head_x(grid_width / 2),
+        head_y(grid_height / 2),
+        grid_width(grid_width),
+        grid_height(grid_height) {}
+       
   void Update();
 
   void GrowBody();
